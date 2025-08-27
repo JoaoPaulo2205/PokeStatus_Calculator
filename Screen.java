e89ea3a9e5f111b6dc92753038c532e78e0d1235
João Paulo <155408484+JoaoPaulo2205@users.noreply.github.com>
2025-08-27 11:19:35 -0300

    novas adições

--- Screen.java
+++ Screen.java
@@ -2,14 +2,18 @@ package pokestats;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Screen extends JFrame {
+
+    JTextField valueAttack, valueDefense, valueHP;
+
     public Screen() {
 
         setSize(330, 670);
         setTitle("Pokemon IV Calc");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(true);
+        setResizable(false);
         setLocationRelativeTo(null);
 
         setLayout(null);
@@ -18,21 +22,77 @@ public class Screen extends JFrame {
 
         calculate.setText("Calculate IV");
         calculate.setBounds(80, 525, 150, 50);
-        calculate.setFont(new Font("Comic Sans", Font.BOLD, 14));
+        calculate.setFont(new Font("Arial", Font.BOLD, 14));
         calculate.setBackground(new Color(240, 240, 240));
         calculate.setForeground(new Color(0, 0, 0));
 
-
         add(calculate);
-        
-        //ImageIcon pokemon = new ImageIcon("C:/Users/Ana Julia/IdeaProjects/PokeStatsCalculator/images/charizard.png");
+        calculate.addActionListener(this::calcularIV);
 
-        
+        valueAttack = new JTextField();
+        valueAttack.setBounds(235, 375, 30, 30);
+        valueAttack.setFont(new Font("Arial", Font.BOLD, 15));
+        valueAttack.setHorizontalAlignment(JTextField.CENTER);
+
+        add(valueAttack);
+
+        valueDefense = new JTextField();
+        valueDefense.setBounds(235, 415, 30, 30);
+        valueDefense.setFont(new Font("Arial", Font.BOLD, 15));
+        valueDefense.setHorizontalAlignment(JTextField.CENTER);
+
+        add(valueDefense);
+
+        valueHP = new JTextField();
+        valueHP.setBounds(235, 455, 30, 30);
+        valueHP.setFont(new Font("Arial", Font.BOLD, 15));
+        valueHP.setHorizontalAlignment(JTextField.CENTER);
+
+        add(valueHP);
+
+        JLabel attack = new JLabel("Insert your Attack level");
+        attack.setBounds(50, 380, 170, 20);
+        attack.setFont(new Font("Arial", Font.PLAIN, 15));
+
+        add(attack);
+
+        JLabel defense = new JLabel("Insert your Defense level");
+        defense.setBounds(50, 420, 170, 20);
+        defense.setFont(new Font("Arial", Font.PLAIN, 15));
+
+        add(defense);
+
+        JLabel hp = new JLabel("Insert your HP level");
+        hp.setBounds(50, 460, 170, 20);
+        hp.setFont(new Font("Arial", Font.PLAIN, 15));
+
+        add(hp);
 
 
 
+
+
+        JLabel imageLabel = new JLabel();
+        ImageIcon pokemon_image = new ImageIcon("C:/Users/user/IdeaProjects/PokeStatsCalculator/src/pokestats/images/gengar.png");
+
+        imageLabel.setIcon(pokemon_image);
+
+        imageLabel.setBounds(80, 40, 200, 200);
+        add(imageLabel);
 
         setVisible(true);
+    }
+
+
+    public void calcularIV(ActionEvent ActionEvent){
+
+        Double attack = Double.parseDouble(valueAttack.getText());
+        Double defense = Double.parseDouble(valueDefense.getText());
+        Double hp = Double.parseDouble(valueHP.getText());
+
+        double result = (((attack + defense + hp) / 45) * 100);
+        String resultIV = String.format("Your Pokemon IV is: %.0f%%", result);
+        JOptionPane.showMessageDialog(null, resultIV, "IV", JOptionPane.INFORMATION_MESSAGE);
     }
 
 }
